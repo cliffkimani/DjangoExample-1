710da506af11c513d46cc6d912ce98e6d2f1e43c
Joe Meiring <jmeiring@localhost.localdomain>
2012-11-28 14:21:52 -0600

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.template import RequestContext,loader
+from django.shortcuts import render_to_response,get_object_or_404
+
+from models import User
+
+def index(request):    
+	"""
+	The landing page
+	"""
+	rc = RequestContext(request)
+	users = User.objects.all()
+	rc['users'] = users
+	return render_to_response('myapp/index.html', rc)
