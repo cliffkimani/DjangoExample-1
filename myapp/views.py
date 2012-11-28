1b434524eb1d09dcd95daaeeae4cb916c5549c75
Joe Meiring <jmeiring@localhost.localdomain>
2012-11-28 16:05:29 -0600

    settings env variable changed

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
-from django.template import RequestContext,loader
-from django.shortcuts import render_to_response,get_object_or_404
+from django.template import RequestContext, loader
+from django.shortcuts import render_to_response, get_object_or_404
 
 from models import User
 
@@ -11,3 +11,4 @@ def index(request):
 	users = User.objects.all()
 	rc['users'] = users
 	return render_to_response('myapp/index.html', rc)
+
